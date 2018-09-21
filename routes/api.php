eaa3d65e2787bb56ec8615c4465a47a992173ef5
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2018-09-21 19:40:28 +0100

    add main routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,28 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/job', [
+	'uses' => 'JobController@postJob',
+	'as' => 'post-job'
+]);
+
+Route::get('/jobs', [
+	'uses' => 'JobController@getJobs',
+	'as' => 'get-jobs'
+]);
+
+Route::put('/job/{job}', [
+	'uses' => 'JobController@updateJob',
+	'as' => 'update-job'
+]);
+
+Route::delete('/job/{job}', [
+	'uses' => 'JobController@deleteJob',
+	'as' => 'delete-job'
+]);
+
+Route::get('/job/{job}', [
+	'uses' => 'JobController@getJob',
+	'as' => 'get-job'
+]);
